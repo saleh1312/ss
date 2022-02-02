287dc88e70444e9fc627779de94d8ba454b6bb4e
saleh1312 <39093306+saleh1312@users.noreply.github.com>
2022-02-02 06:36:47 +0200

    Update trainusingLSTM.py

--- trainusingLSTM.py
+++ trainusingLSTM.py
@@ -1,15 +1,11 @@
+
+
+### importing the libraries
 import pandas as pd
 import pickle
 from urllib.parse import urlparse, urlencode
-df = pd.read_csv('finaldata.csv')
-print(df.head())
-x= df['url']
-y = df['label']
-#print(x.shape)
-#print(y.shape)
 
 import tensorflow as tf
-
 from tensorflow.keras.layers import Embedding
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
@@ -17,18 +13,27 @@ from tensorflow.keras.preprocessing.text import one_hot
 from tensorflow.keras.layers import LSTM
 from tensorflow.keras.layers import Dense
 
-voc_size = 10000
-messages = x.copy()
-#print(messages[1])
-#messages.reset_index(inplace=True)
 
 import nltk
 import re
 from nltk.corpus import stopwords
-
-tf.random.set_seed(42)
-
 from nltk.stem.porter import PorterStemmer
+import numpy as np
+
+###set the random seed
+tf.random.set_seed(42)
+np.random.seed(42)
+
+
+
+###read and process the data
+df = pd.read_csv('finaldata.csv')
+x= df['url']
+y = df['label']
+
+voc_size = 10000
+messages = x.copy()
+
 ps = PorterStemmer()
 corpus = []
 for i in range(0, len(messages)):
@@ -37,48 +42,32 @@ for i in range(0, len(messages)):
     review = review.split()
     review=' '.join(review)
     corpus.append(review)
-#print(corpus[11])
-onehot_repr=[one_hot(words,voc_size)for words in corpus]
-#print(onehot_repr[1])
 
+onehot_repr=[one_hot(words,voc_size)for words in corpus]
 sent_length = 50
 embedded_docs= pad_sequences(onehot_repr,padding='pre',maxlen=sent_length)
-print(embedded_docs[1])
 
+x_final = np.array(embedded_docs)
+y_final  = np.array(y)
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test = train_test_split(x_final,y_final,test_size=0.20,random_state=42)
+
+
+#make the model and train it
 embedding_vector_features=100
 model = Sequential()
 model.add(Embedding(voc_size,embedding_vector_features,input_length=sent_length))
 model.add(LSTM(100))
 model.add(Dense(1,activation='sigmoid'))
 model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
-print(model.summary())
-
-#print(len(embedded_docs))
-import numpy as np
-np.random.seed(42)
-x_final = np.array(embedded_docs)
-y_final  = np.array(y)
-
-print(x_final.shape)
-
-from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test = train_test_split(x_final,y_final,test_size=0.20,random_state=42)
 
 
 model.fit(x_train,y_train,validation_data=(x_test,y_test),epochs=10,batch_size=64)
-
-#y_pred = model.predict_classes(x_test)
-#y_pred= model.predict_classes(x_test)
 
 y_pred=model.predict(x_test) 
 classes_y=np.round(y_pred).astype(int)
 
 model.save('model.h5')
-#pickle.dump(model, open('model6.pkl','wb'))
-
-
-# with open("model5.pkl", "wb") as file:
-#      pickle.dump(model, file)
 
 
     
